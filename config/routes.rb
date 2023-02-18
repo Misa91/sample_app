0e4382cfaf3f58858f6a05313c930e46a84a9732
Misa91 <101438118+Misa91@users.noreply.github.com>
2023-02-18 02:50:12 +0000

    [Finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'   #名前が一致していれば省略可能
-  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'  #投稿機能で入力内容を新規保存できるようにした
   get 'lists' => 'lists#index'    #一覧機能で加えた
   get 'lists/:id' => 'lists#show', as: 'list'
   #どの投稿データを表示するかを判別するため、投稿データのidもURLに含める
-  #'lists#show'の設定を、listとして利用する
-
+  #'lists#show'の設定を、listとして利用する(名前付きルート)
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  #lists/◯◯/editのすべてのURLが対象
+  patch 'lists/:id' => 'lists#update', as: 'update_list' #編集内容を保存できるようにした
 end
